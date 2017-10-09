440230a71e78f233658ae721bc7666c2fc5d2346
pwrstudio <pwrpntr@gmail.com>
2017-10-09 21:33:53 +0200

    Changes to the works view:
    - Fixed the captions to be same width as image container by making the caption container block element (div) + width=100%
    - Only show the first image even if there are multiple

--- src/components/works.vue
+++ src/components/works.vue
@@ -3,9 +3,8 @@
     <div class="scroll" :style="scrollSize" v-images-loaded:on.always="resizeScroll">
       <!-- Nested for loops takes all images in a project as equals. Could allow for grouping? Not sure about descriptions yet -->
       <div v-for="(item, index) in main.posts" class="work">
-        <img v-if="item.acf.images.length === 1" :src='item.acf.images[0].image.sizes["pwr-large"]' ref="workImg" :id="item.id"/>
-        <img v-else v-for="iitem in item.acf.images" :src='iitem.image.sizes["pwr-large"]' ref="workImg" :id="item.id"/>
-        <p class="text" :class="{ more: main.showMore }" v-html="item.acf.text" :style="{ width: imageWidths[0] + 'px' }"></p>
+        <img :src='item.acf.images[0].image.sizes["pwr-large"]' ref="workImg" :id="item.id"/>
+        <div class="text" v-if="main.showMore" v-html="item.acf.text"></div>
       </div>
     </div>
   </div>
@@ -96,17 +95,16 @@ export default {
     }
     .text {
       position: absolute;
-      display: none;
       bottom: 0;
       height: $line-height * 2;
+      line-height: $line-height * 2;
       padding: 2px 0 0 $margin-sides;
       background: $background-color;
       z-index: 3;
       color: $green;
       width: 100%;
-      &.more {
-        display: block;
-      }
+      white-space: nowrap;
+      text-overflow: ellipsis;
     }
   }
 }
